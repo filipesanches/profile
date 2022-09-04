5f2699be703c9c403d647f02293df93e3a79f281
Filipe Sanches <53585862+filipesanches@users.noreply.github.com>
2022-09-04 18:02:36 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 const texto = document.querySelector("p");
+const texto1 = document.querySelector("div:nth-child(2) span");
+
+
 function maquinaDeEscrever(texto) {
   const txtArray = texto.innerText.split("");
   texto.innerHTML = "";
@@ -7,3 +10,4 @@ function maquinaDeEscrever(texto) {
   });
 }
 maquinaDeEscrever(texto);
+maquinaDeEscrever(texto1);

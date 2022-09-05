85870f1a33f387e4e25e7b60e4e0f33d02bd6286
Filipe Sanches <53585862+filipesanches@users.noreply.github.com>
2022-09-05 16:02:24 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function maquinaDeEscrever(texto) {
 maquinaDeEscrever(texto);
 
 
-function openTab(evt, cityName) {
+function openTab(evt, idName) {
   var i, tabcontent, tablinks;
   tabcontent = document.querySelectorAll("section");
   for (i = 0; i < tabcontent.length; i++) {
@@ -17,8 +17,21 @@ function openTab(evt, cityName) {
   }
   tablinks = document.querySelectorAll(".tab");
   for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.background = '#ccc';
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(idName).style.display = "block";
   evt.currentTarget.className += " active";
+  if(idName == 'home'){
+    document.querySelector(`#${idName} div`).style.background = '#8ea6bd';
+    document.querySelector(`.active`).style.background = '#6589aa';
+  }
+  if(idName == 'skill'){
+    document.querySelector(`#${idName} div`).style.background = '#65aa76';
+    document.querySelector(`.active`).style.background = 'green';
+  }
+  if(idName == 'projects'){
+    document.querySelector(`#${idName} div`).style.background = '#6665aa';
+    document.querySelector(`.active`).style.background = '#27273d';
+  }
 }

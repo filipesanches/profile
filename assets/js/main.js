980b83fed4d34ae9a78aa1043133b4755e04db25
Filipe Sanches <53585862+filipesanches@users.noreply.github.com>
2022-09-05 20:46:54 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
+//Anima Background
+const conteudo = document.querySelector('.conteudo-anima')
+function AnimaBg(tag){
+  for(let i = 0; i <= 50; i++){
+    const blocks = document.createElement('div');
+    blocks.classList.add('block')
+    tag.appendChild(blocks)
+  }
+}
+AnimaBg(conteudo)
+function animateBlocks(){
+  anime({
+  targets: '.block',
+  translateX: () => anime.random(-700,700),
+  translateY: () => anime.random(-700,700),
+  scale: () => anime.random(1,5),
+
+  easing: 'linear',
+  duration: 4000
+});
+}
+animateBlocks()
+
+//Efeito Maquina de Escrver
 const texto = document.querySelector("p");
 function maquinaDeEscrever(texto) {
   const txtArray = texto.innerText.split("");
@@ -8,7 +32,7 @@ function maquinaDeEscrever(texto) {
 }
 maquinaDeEscrever(texto);
 
-
+//Paj=gination
 function openTab(evt, idName) {
   var i, tabcontent, tablinks;
   tabcontent = document.querySelectorAll("section");
@@ -17,21 +41,20 @@ function openTab(evt, idName) {
   }
   tablinks = document.querySelectorAll(".tab");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.background = '#ccc';
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(idName).style.display = "block";
   evt.currentTarget.className += " active";
-  if(idName == 'home'){
-    document.querySelector(`#${idName} div`).style.background = '#8ea6bd';
-    document.querySelector(`.active`).style.background = '#6589aa';
-  }
-  if(idName == 'skill'){
-    document.querySelector(`#${idName} div`).style.background = '#65aa76';
-    document.querySelector(`.active`).style.background = 'green';
-  }
-  if(idName == 'projects'){
-    document.querySelector(`#${idName} div`).style.background = '#6665aa';
-    document.querySelector(`.active`).style.background = '#27273d';
-  }
+  // if(idName == 'home'){
+  //   document.querySelector(`#${idName} div`).style.background = '#8ea6bd';
+  //   document.querySelector(`.active`).style.background = '#6589aa';
+  // }
+  // if(idName == 'skill'){
+  //   document.querySelector(`#${idName} div`).style.background = '#65aa76';
+  //   document.querySelector(`.active`).style.background = 'green';
+  // }
+  // if(idName == 'projects'){
+  //   document.querySelector(`#${idName} div`).style.background = '#6665aa';
+  //   document.querySelector(`.active`).style.background = '#27273d';
+  // }
 }
